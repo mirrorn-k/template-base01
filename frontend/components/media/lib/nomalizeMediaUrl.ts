07619622948ef15a58278ff51046785b0f89b56c
mirrorn-k <mirrorn.3202@addnew.jp>
2025-12-23 16:55:08 +0900

    kimotokkのenv修正

--- frontend/components/media/lib/nomalizeMediaUrl.ts
+++ frontend/components/media/lib/nomalizeMediaUrl.ts
@@ -4,6 +4,22 @@ import { tMedia } from "@/types/ttnouMap";
 
 const normalizeMediaUrl = (media?: tMedia) => {
   return media;
+
+  /*
+  if (!media || !media.url) {
+    return media;
+  }
+
+  if (idProd) {
+    // 本番環境の場合
+    media.url = media.url.replace("https://ma-plus-backend.ttnou.com", "");
+  } else {
+    media.url = media.url.replace("http://localhost:8102", "");
+    media.url = media.url.replace("https://ma-plus-backend.ttnou.com", ""); // バックアップからのデータを直接表示できるようにするために必要
+  }
+
+  return media;
+  */
 };
 
 export default normalizeMediaUrl;
